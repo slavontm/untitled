80292836af3d298d2541cf614afd1bc07e9a5ca8
slavon <nslava@gmail.com>
2023-03-01 18:07:49 +0300

    variable renaming

--- pflevels.java
+++ pflevels.java
@@ -11,13 +11,13 @@ public class pflevels {
         System.out.println(PF+" 5th level "+"fast"); //
         System.out.println("\n\nТаблица\n\n");
 
-        String[] seasons  = {"L", "R", "D"};
+        String[] hands  = {"L", "R", "D"};
         int k=1;
                for (int i=1;i<=10;i++) {
                     for (int j=1;j<=10;j++) {
                         int c=(int)((Math.random() * (2  + 1) + 0));
 
-                        System.out.printf("%6d %s", (k++),seasons[c]);
+                        System.out.printf("%6d %s", (k++),hands[c]);
                     }
                     System.out.println();
                 }
